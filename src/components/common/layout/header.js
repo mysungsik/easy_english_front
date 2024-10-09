fb851a8b4fc72a7eb24eaa7c19eb7bc22d812220
DESKTOP-HPCIERD\mschoi <audtlr93@naver.com>
2024-10-09 11:48:19 +0900

    [Design] 메인페이지 디자인 변경

--- src/components/common/layout/header.js
+++ src/components/common/layout/header.js
@@ -19,58 +19,60 @@ const Header = () => {
         }, 400);
     };
     return (
-        <header className={`${navId != "" ? style['show'] : style['hide']}`}>
-            {/* 헤더 상단 */}
-            <div className={`${style['header-top']} `}>
-                {/* 로고 및 네비게이션 파트 */}
-                <div className={`${style['header-top-left']}`}>
-                    <Link className={`${style['left-logo']}`} to={"/"}>
-                        <img className={`${style['logo']}`} src="/logo/logo_main.png" alt="logo"/>
-                    </Link>
-                    <div className={`${style['left-nav']}`}>
-                        <ul className={'d-flex'}>
-                            <li className={`fs-20 pointer`} onMouseEnter={() => handleMouseEnter("learn")} onMouseLeave={handleMouseLeave}>
-                                학습하기
-                            </li>
-                            <li className={`fs-20 pointer`} onMouseEnter={() => handleMouseEnter("game")} onMouseLeave={handleMouseLeave}>
-                                게임하기
-                            </li>
-                        </ul>
+        <div className={`${style['header-background']}`}>
+            <header className={`${navId != "" ? style['show'] : style['hide']}`}>
+                {/* 헤더 상단 */}
+                <div className={`${style['header-top']} `}>
+                    {/* 로고 및 네비게이션 파트 */}
+                    <div className={`${style['header-top-left']}`}>
+                        <Link className={`${style['left-logo']}`} to={"/"}>
+                            <img className={`${style['logo']}`} src="/logo/logo_main.png" alt="logo"/>
+                        </Link>
+                        <div className={`${style['left-nav']}`}>
+                            <ul className={'d-flex'}>
+                                <li className={`fs-20 pointer`} onMouseEnter={() => handleMouseEnter("learn")} onMouseLeave={handleMouseLeave}>
+                                    학습하기
+                                </li>
+                                <li className={`fs-20 pointer`} onMouseEnter={() => handleMouseEnter("game")} onMouseLeave={handleMouseLeave}>
+                                    게임하기
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* 유저 로그인 파트 */}
+                    <div className={`${style['header-top-right']}`}>
+                        <Link to={"/login"} className={`btn-small__blue loginBtn`}> 로그인 </Link>
                     </div>
                 </div>
-                {/* 유저 로그인 파트 */}
-                <div className={`${style['header-top-right']}`}>
-                    <Link to={"/login"} className={`btn-small__blue`}> 로그인 </Link>
+                {/* 헤더 하단 슬라이드 */}
+                <div
+                    className={`${style['header-bot']} base__blue`}
+                    onMouseEnter={() => handleMouseEnter(navId)}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    {navId === "learn" && (
+                        <ul>
+                            <li>
+                                <Link to={"/learn/today"} className="fc__white"> 오늘의 학습 </Link>
+                            </li>
+                            <li>
+                                <Link to={"/learn/review"} className="fc__white"> 복습 </Link>
+                            </li>
+                            <li>
+                                <Link to={"/learn/wordbook"} className="fc__white"> 나의 단어장 </Link>
+                            </li>
+                        </ul>
+                    )}
+                    {navId === "game" && (
+                        <ul>
+                            <li>
+                                <Link to={"/game/hangman"} className="fc__white"> 행맨게임 </Link>
+                            </li>
+                        </ul>
+                    )}
                 </div>
-            </div>
-            {/* 헤더 하단 슬라이드 */}
-            <div
-                className={`${style['header-bot']} base__blue`}
-                onMouseEnter={() => handleMouseEnter(navId)}
-                onMouseLeave={handleMouseLeave}
-            >
-                {navId === "learn" && (
-                    <ul>
-                        <li>
-                            <Link to={"/learn/today"} className="fc__white"> 오늘의 학습 </Link>
-                        </li>
-                        <li>
-                            <Link to={"/learn/review"} className="fc__white"> 복습 </Link>
-                        </li>
-                        <li>
-                            <Link to={"/learn/wordbook"} className="fc__white"> 나의 단어장 </Link>
-                        </li>
-                    </ul>
-                )}
-                {navId === "game" && (
-                    <ul>
-                        <li>
-                            <Link to={"/game/hangman"} className="fc__white"> 행맨게임 </Link>
-                        </li>
-                    </ul>
-                )}
-            </div>
-        </header>
+            </header>
+        </div>
     )
 }
 
--- src/components/dashboard/dashboard-score.js
+++ src/components/dashboard/dashboard-score.js
@@ -1,87 +1,99 @@
+import { useEffect, useState } from "react"
 import style from "./dashboard-score.module.css"
 
 const DashboardScore = () =>{
+
+    const [hangmanScore, setHangmanScore] = useState([]);
+    const [studyScore, setStudyScore] = useState([]);
+
+    useEffect(()=>{
+        setHangmanScore([
+            {nickname : 'user1', date : '2024-09-09', score : 129},
+            {nickname : 'user2', date : '2024-09-09', score : 129},
+            {nickname : 'user3', date : '2024-09-09', score : 129},
+            {nickname : 'user4', date : '2024-09-09', score : 129},
+            {nickname : 'user5', date : '2024-09-09', score : 129}
+        ])
+
+        setStudyScore([
+            {nickname : 'user1', date : '2024-09-09', score : 521},
+            {nickname : 'user2', date : '2024-09-09', score : 520},
+            {nickname : 'user3', date : '2024-09-09', score : 129},
+            {nickname : 'user4', date : '2024-09-09', score : 129},
+            {nickname : 'user5', date : '2024-09-09', score : 129}
+        ])
+    },[])
+
     return (
         <div className={`${style['dashboard-score']}`}>
-            <div>
-                <div>
-                    행맨 최고점수 TOP 5
-                </div>
-                <table>
-                    <thead className={`base__blue`}>
-                        <td  className={`fc__white`}>
-                            닉네임
-                        </td>
-                        <td  className={`fc__white`}>
-                            날짜
-                        </td>
-                        <td  className={`fc__white`}>
-                            점수
-                        </td>
-                    </thead>
-                    <tr>
-                        <td>
-                            명식
-                        </td>
-                        <td>
-                            2024-04-12
-                        </td>
-                        <td>
-                            132
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            지현
-                        </td>
-                        <td>
-                            2024-04-15
-                        </td>
-                        <td>
-                            132
-                        </td>
-                    </tr>
-                </table>
+            <div className={`${style['ranking-div']}`}>
+                <p className={`fs-14 mb-4`}> Easy Ranking </p>
+                <p className={`fs-20`}> 최고 점수에 도전하세요! </p>
             </div>
-            <div>
+            <div className={`${style['scoreboard-div']}`}>
                 <div>
-                    30일 연속 최고 스터디맨 TOP 5
+                    <div>
+                        행맨 최고점수 TOP 5
+                    </div>
+                    <table>
+                        <thead className={`base__blue`}>
+                            <td  className={`fc__white`}>
+                                닉네임
+                            </td>
+                            <td  className={`fc__white`}>
+                                날짜
+                            </td>
+                            <td  className={`fc__white`}>
+                                점수
+                            </td>
+                        </thead>
+                        {hangmanScore.map((item)=>(
+                            <tr>
+                                <td>
+                                    {item.nickname}
+                                </td>
+                                <td>
+                                    {item.date}
+                                </td>
+                                <td>
+                                    {item.score}
+                                </td>
+                            </tr>
+                        ))}
+                    </table>
                 </div>
-                <table>
-                    <thead className={`base__blue`}>
-                        <td  className={`fc__white`}>
-                            닉네임
-                        </td>
-                        <td  className={`fc__white`}>
-                            날짜
-                        </td>
-                        <td  className={`fc__white`}>
-                            새로배운 단어
-                        </td>
-                    </thead>
-                    <tr>
-                        <td>
-                            명식
-                        </td>
-                        <td>
-                            2024-04-12
-                        </td>
-                        <td>
-                            132
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            지현
-                        </td>
-                        <td>
-                            2024-04-15
-                        </td>
-                        <td>
-                            132
-                        </td>
-                    </tr>
-                </table>
+                <div>
+                    <div>
+                        30일 연속 최고 스터디맨 TOP 5
+                    </div>
+                    <table>
+                        <thead className={`base__blue`}>
+                            <td  className={`fc__white`}>
+                                닉네임
+                            </td>
+                            <td  className={`fc__white`}>
+                                날짜
+                            </td>
+                            <td  className={`fc__white`}>
+                                새로배운 단어
+                            </td>
+                        </thead>
+                        {studyScore.map((item)=>(
+                            <tr>
+                                <td>
+                                    {item.nickname}
+                                </td>
+                                <td>
+                                    {item.date}
+                                </td>
+                                <td>
+                                    {item.score}
+                                </td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+
             </div>
         </div>
     )
