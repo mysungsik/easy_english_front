0900a3e9c6feadfae8dbbaa9760afcf7bd64f2dc
DESKTOP-HPCIERD\mschoi <audtlr93@naver.com>
2024-10-20 17:41:07 +0900

    [Design] 오늘의 학습 버튼 추가

--- src/components/learn/learn-today-main.js
+++ src/components/learn/learn-today-main.js
@@ -1,52 +1,108 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import style from "./learn-today.module.css"
+import axiosInstance from "../../config/axiosConfig"
+import UserContext from "../../context/userContext"
 
 
 const LearnTodayMain = () =>{
-
     const [question, setQuestion] = useState({})
     const [hintLevel, setHintLevel] = useState(0)
+    const {user} = useContext(UserContext)
+    const [answer, setAnswer] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    console.log(loading)
 
     useEffect(()=>{
-        setQuestion({
-            word: 'took',
-            word_answer: '빼다',
-            example: 'I took off mt rings to wash my hands and forgot to took them back on',
-            example_answer: '반지를 빼고 손을 씻고 나서 다시 끼는 것을 깜빡했어'
-        })
+        getWord(false)
     },[])
 
+    useEffect(()=>{
+        showHint()
+    },[hintLevel])
+
+    // 데이터가져왔으면 Loading 상태 종료
+    useEffect(()=>{
+        setLoading(false)
+    },[question])
+
+    // 데이터 가져오기
+    const getWord = async (isCorrect) =>{
+        setLoading(true)
+        // 초기 데이터 가져오기
+        if (!isCorrect){
+            const response = await axiosInstance.get(`/learn/getCurrentWordForMemeber?memberNo=${user.memberNo}`)
+                setQuestion(response)
+        }
+
+        // 정답 제출시 다음 데이터 가져오기
+        if (isCorrect){
+            const response = await axiosInstance.get(`/learn/getCurrentWordForMemeber?memberNo=${user.memberNo}&currentWordId=${question.wordId}`)
+            setQuestion(response)
+        }
+    }
+
+    
+    // word의 길이에 따른 input 너비 계산 함수
+    const getInputWidth = (wordLength) => {
+        return wordLength * 12 + 'px'; // 1글자당 10px로 가정
+    };
+
     // 힌트생성
-    const showHint = ()=>{
-        if (hintLevel < 1 ){
-            setHintLevel(hintLevel + 1)
+    const handleHintCnt = ()=>{
+        hintLevel < 2 ? setHintLevel(hintLevel + 1) : setHintLevel(0)
+    }
+
+    const showHint = () =>{
+        if (hintLevel === 0){
+            setAnswer("")
+        }
+        else if (hintLevel === 1 ){
+            setAnswer(question.wordSpell.substring(0,1))
         }else{
-            setHintLevel(0)
-            // TODO: 다음 문제로 넘어가기
+            setAnswer(question.wordSpell)
         }
     }
 
     // 음성 출력
     const speechHint = ()=>{
         const utterance = new SpeechSynthesisUtterance();
-        utterance.text = question.example;
+        utterance.text = question.exampleSentence;
         utterance.lang = 'en-US'; // 언어 설정 (미국 영어)
         utterance.rate = 1.0; // 속도 설정 (1이 기본)
         utterance.pitch = 1.0; // 음높이 설정 (1이 기본)
         speechSynthesis.speak(utterance);
     }
-    
 
-    // word의 길이에 따른 input 너비 계산 함수
-    const getInputWidth = (wordLength) => {
-        return wordLength * 10 + 'px'; // 1글자당 10px로 가정
-    };
+    // 정답 핸들러
+    const handleAnswer = (e) =>{
+        const value = e.target.value
+        setAnswer(value)
+    }
+
+    // 정답 제출
+    const submitAnswer = () =>{
+        if (answer.toLowerCase() == question.wordSpell.toLowerCase()){
+            getWord(true)
+            resetInput()
+            alert("정답입니다!")
+        }
+        else{
+            alert("아까워요! 다시 풀어보세요!")
+        }
+    }
+
+    const resetInput = () =>{
+        setAnswer("")
+        setHintLevel(0)
+    }
 
     return (
         <section className={`${style['learntoday-main-section']}`}>
+            {loading && <div> 다음 문장 생성중... </div>}
             <div className={`d-flex ${style['word-info-div']}`}>
                 <div className={`d-flex`}>
-                    <p>레벨 2</p>
+                    <p>레벨 {question.wordId}</p>
                     <button> 단어 저장</button>
                 </div>
                 <div>
@@ -54,36 +110,45 @@ const LearnTodayMain = () =>{
                 </div>
             </div>
             <div className={`${style['word-question-div']} base__lorange`}>
-                <p>
-                    {question.example_answer}
-                </p>
-                <p>
-                    {('word' in question) ? question.example.split(' ').map((word, index) => {
-                        // 띄어쓰기로 분류 후 match 
-                        // new RegExp('값', [옵션]) -> 'i' 는 대소문자 구별하지않음 옵션
-                        const match = word.match(new RegExp(question.word, 'i'));
-                        return (
-                            <span key={index}>
-                                {match ? (
-                                    // match 되면 input 아니라면 기본 word 표시
-                                    <input type="text" style={{ width: getInputWidth(question.word.length) }}/>
-                                ) : (
-                                    <span>{word}</span>
-                                )}
-                                {' '}
-                            </span>
-                        );
-                    }) : ''}
-                </p>
+                <div>
+                    <p>
+                        {question.exampleMean}
+                    </p>
+                    <p>
+                        {('wordSpell' in question) ? question.exampleSentence.split(' ').map((word, index) => {
+                            // 띄어쓰기로 분류 후 match 
+                            // new RegExp('값', [옵션]) -> 'i' 는 대소문자 구별하지않음 옵션
+                            const match = word.match(new RegExp(question.wordSpell, 'i'));
+                            return (
+                                <span key={index}>
+                                    {match ? (
+                                        // match 되면 input 아니라면 기본 word 표시
+                                        <input type="text" className={`${style['questionInput']}`} 
+                                            style={{ width: getInputWidth(question.wordSpell.length) }} disabled={true} value={answer}/>
+                                    ) : (
+                                        <span>{word}</span>
+                                    )}
+                                    {' '}
+                                </span>
+                            );
+                        }) : ''}
+                    </p>
+                </div>
+                <div>
+                    <label htmlFor="answer">정답 입력</label>
+                    <input type="text" className={`${style['answerInput']}`} name="answer" value={answer} onChange={handleAnswer}/>
+                    <button onClick={submitAnswer}> 제출 </button>
+                </div>
             </div>
 
             <div className={`d-flex ${style['word-hint-div']}`}>
-                {hintLevel == 0 && <button onClick={()=>showHint()}>힌트</button>}
-                {hintLevel == 1 && <button onClick={()=>showHint()}>정답보기</button>}
+                {hintLevel == 0 && <button onClick={()=>handleHintCnt()}>힌트</button>}
+                {hintLevel == 1 && <button onClick={()=>handleHintCnt()}>정답보기</button>}
+                {hintLevel == 2 && <button onClick={()=>handleHintCnt()}>다시풀어보기</button>}
                 
                 <button onClick={()=>speechHint()}> 음성힌트 </button>
             </div>
-        </section>        
+        </section>
     )
 }
 
