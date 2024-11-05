28d2878e123403eedfbdd2dae42330e592d86577
DESKTOP-HPCIERD\mschoi <audtlr93@naver.com>
2024-11-05 16:07:10 +0900

    [Feat] Vercel Analytics 추가

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import Layout from './components/common/layout/layout';
 import { UserContextProvider } from './context/userContext';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <BrowserRouter>
+        <Analytics/>
         <UserContextProvider>
           <Layout>
             <App />
